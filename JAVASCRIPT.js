ed5ea60c329d9df5a299732ed8d21fea5cb3768b
Mohammad Amer <msadeq_eng@yahoo.com>
2021-01-31 17:10:33 +0200

    added js functions and css

--- JAVASCRIPT.js
+++ JAVASCRIPT.js
@@ -3,11 +3,18 @@ var status;
 while ( acceptorno !=='y' && acceptorno !== 'n'){
     acceptorno = prompt("are you interested in a FULL Software Development scholarship? y or n");
 }
+
+function getName() {
+    var userName = prompt('Enter your name?');
+    console.log(userName);
+    document.write(userName);
+    }
 if (acceptorno==="y"){
-    var age= prompt('What is your age?')
-    var times = prompt('how many scholarship you want to attend?')
+    var age= prompt('What is your age?');
+    var times = prompt('how many scholarship you want to attend?');
     if (age >=18 && age<=35 ) {
-        status = "** CONGRATS; You are eligable to the scholarship **"
+        status = "** CONGRATS; You are eligable to the scholarship **";
+        getName()
         confirm("Are you happy with this javascript");
         alert("THANK YOU");
     } else {
@@ -31,6 +38,7 @@ var result =''
 for (var i = 0; i < times ; i++) {
     result = result + statusimage;
   }
+
 document.write(status)
 document.write(result)
 document.getElementById("statusof").innerText= age;
